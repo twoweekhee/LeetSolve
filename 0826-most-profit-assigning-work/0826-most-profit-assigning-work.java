614beb66c6650874cd72aeb84e91e7cf11569b89
JuHee_Lee <108851055+twoweekhee@users.noreply.github.com>
2024-06-18 11:30:07 +0900

    Time: 583 ms (9.82%), Space: 46.1 MB (18.3%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,18 +1,24 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        Map<Integer, Integer> map = new HashMap<>();
+        List<int[]> list = new ArrayList<>();
         int maxProfit = 0;
 
         for (int i = 0; i < profit.length; i++) {
-            map.put(profit[i], difficulty[i]);
+            int[] ex = {profit[i], difficulty[i]};
+           list.add(ex);
         }
-        List<Integer> profitList = new ArrayList<>(map.keySet());
-        Collections.sort(profitList, Collections.reverseOrder());
+        Collections.sort(list,  new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(b[0], a[0]); 
+            }
+        });
 
         for (int i = 0; i < worker.length; i++) {
-            for(int p : profitList){
-                if (map.get(p) <= worker[i]){
-                    maxProfit = maxProfit + p;
+            for (int j = 0; j < list.size(); j++) {
+                int[] ex = list.get(j);
+                if (ex[1] <= worker[i]) {
+                    maxProfit += ex[0];
                     break;
                 }
             }
